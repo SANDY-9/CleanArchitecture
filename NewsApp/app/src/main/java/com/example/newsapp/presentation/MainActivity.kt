e63f18b0bfce65410499535767a7dacf643291ea
SANDY <nnal0256@naver.com>
2024-02-27 20:04:39 +0900

    [NewsApp] presentation layer: NavGraph 추가

--- NewsApp/app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.example.newsapp
+package com.example.newsapp.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,34 +15,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.domain.use_cases.AppEntryUseCases
-import com.example.newsapp.presentation.onboarding.OnBoardingScreen
-import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.example.newsapp.presentation.navgraph.NavGraph
 import com.example.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect {
-                Log.e("확인", "onCreate: :$it", )
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
             }
         }
-
         setContent {
             NewsAppTheme {
                 Surface(
@@ -50,10 +39,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                        val viewModel: OnBoardingViewModel = hiltViewModel()
-                        OnBoardingScreen(
-                            event = viewModel::onEvent
-                        )
+                        val startDestination = viewModel.startDestination
+                        NavGraph(startDestination = startDestination)
                     }
                 }
             }
